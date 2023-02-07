09cd3eb4334fae8b816f69a718feac40f24f060a
samunie32 <123445424+samunie32@users.noreply.github.com>
2023-02-07 09:37:06 +0200

    jj

--- kirjautuminen.php
+++ kirjautuminen.php
@@ -5,23 +5,20 @@ $password = $_POST["psw"];
 // Tietokannan yhteyden muodostaminen
 $servername = "localhost";
 $dbusername = "root";
-$dbpassword = "Juures2";
-$dbname = "fillaritsyg\xC3\xA4";
+$dbpassword = "1234";
+$dbname = "fillaritsygä";
 
-// Luodaan yhteys tietokantaan
+
 $conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
 
-// Tarkistetaan yhteys
 if (!$conn) {
     die("Tietokantayhteys epäonnistui: " . mysqli_connect_error());
 }
 
-// Valitaan tietokannan taulu ja haetaan käyttäjän tiedot
 $sql = "SELECT käyttäjätunnus, salasana FROM käyttäjä WHERE käyttäjätunnus='$username'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    // Käyttäjä löytyi, tarkistetaan salasana
     $row = mysqli_fetch_assoc($result);
     if($row["salasana"] == $password){
         header("Location: index.html");
@@ -33,9 +30,9 @@ if (mysqli_num_rows($result) > 0) {
     echo "Käyttäjää ei löytynyt. Yritä uudelleen.";
 }
 
-// Suljetaan tietokantayhteys
+
 mysqli_close($conn);
-?>
+
 
 
 
