1031248ba9545af16ba811a3b1333832fda536fc
samunie32 <123445424+samunie32@users.noreply.github.com>
2023-02-13 16:36:42 +0200

    uutta

--- rekisteröityminen.php
+++ rekisteröityminen.php
@@ -4,9 +4,9 @@ $username = "root";
 $password = "1234";
 $dbname = "fillaritsyga";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
